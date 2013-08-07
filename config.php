b2cf913dec0ee669e20f69ffe8aa9028dd889a40
cceballosq <cceballosq@gmail.com>
2013-08-07 11:24:11 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+//home
+/*
+define('requiresBD', true);
+define('DB_Engine', 'mysqli');
+define('DB_Server', 'localhost');
+define('DB_name', 'login');
+define('DB_User', 'root');
+define('DB_Password', '0000');
+define('DB_Port', false);
+define('Path', "http://localhost/proyecto_1/prototipo2/");
+*/
+
+//servicio
+
+define('requiresBD', true);
+define('DB_Engine', 'mysqli');
+define('DB_Server', '148.213.9.64');
+define('DB_name', 'flavor');
+define('DB_User', 'root');
+define('DB_Password', 'cactus');
+define('DB_Port', false);
+define('Path', "http://148.213.9.64:84/servicio/miguel/prototipo2/");
+
+?>
