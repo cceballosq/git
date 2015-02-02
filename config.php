f5838bc94308eafb136bb75f6ac31f0835dcd6fc
cmc:w7:home <cceballosq@gmial.com>
2015-02-01 19:39:10 -0600

    nuevo45

--- config.php
+++ config.php
@@ -11,7 +11,7 @@ define('DB_Port', false);
 define('Path', "http://localhost/proyecto_1/prototipo2/");
 */
 
-//servicio ip2
+//servicio ip2 nuevo
 
 define('requiresBD', true);
 define('DB_Engine', 'mysqli');
