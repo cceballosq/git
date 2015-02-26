1750a28537f2eb198bcc3c562c00e64c1aecbdb2
cm:ceballos <cceballosq@gmail.com>
2015-02-26 17:47:31 -0600

    ds

--- config.php
+++ config.php
@@ -13,7 +13,7 @@ define('Path', "http://localhost/proyecto_1/prototipo2/");
 
 //linea master
 //linea cmc
-
+//master
 
 //http://git-scm.com/book/es/v1/Ramificaciones-en-Git-Reorganizando-el-trabajo-realizado
 define('requiresBD', true);
