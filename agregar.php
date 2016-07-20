90fbc8866460b22dfd14af3737997c9ae3eda835
cmc:w7:home <cceballosq@gmial.com>
2016-07-19 22:07:55 -0500

    ALGUNOS COMENTARIOS EN GITIGNORE

--- agregar.php
+++ agregar.php
@@ -1,17 +1,17 @@
 <?php 
-AGREGAR CAMBIOS TODOS LOS CAMBIOS :
-#git add .
-AGREGAR UN CAMBIO EN PARTICULAS :
-#git add nombreArchivo.php
+//AGREGAR CAMBIOS TODOS LOS CAMBIOS :
+git add .
+#AGREGAR UN CAMBIO EN PARTICULAS :
+git add nombreArchivo.php
 
-COMENTAR LOS CAMBIOS :
-#git commit -m "comentario de cambio"
-SUBIR CAMBIO A LA RAMA :
-#git push origin rama
+#COMENTAR LOS CAMBIOS :
+git commit -m "comentario de cambio"
+#SUBIR CAMBIO A LA RAMA :
+git push origin rama
 
-AGREGAR CAMBIO A LA MASTER LOCAL :
-#git checkout master
-#git merge ramaconloscambios
+#AGREGAR CAMBIO A LA MASTER LOCAL :
+git checkout master
+git merge ramaconloscambios
 
 AGRAGAR CAMBIO A LA MASTER SERVICOR :
 #git push origin master  //ya una vez agregados a la local solo se hace push master server
--- gitignore.php
+++ gitignore.php
@@ -1,9 +1,11 @@
 <?php 
-	ASIGNAR UN ARCHIVO A .gitignore
-	#archivo.php
-	ASIGNAR UN CARPETA :
-	#logs/*  /el contenido de la carpeta logs sera ignorado
-	ASIGNAR UN TIPO DE ARCHIVO :
-	#*.log
+	/* la configuracion debe estar guardada en gitignore en la raiz del proyecto */
+	#ASIGNAR UN ARCHIVO A .gitignore
+	archivo.php
+	#ASIGNAR UN CARPETA :
+	/*el contenido de la carpeta logs sera ignorado esta debe de estar vacia*/
+	logs/*  
+	#ASIGNAR UN TIPO o EXTENCION DE ARCHIVO :
+	*.log
 
  ?>
